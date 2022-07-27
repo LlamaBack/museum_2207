9b0c54f7e4fff4c0afeb519221623523b5194000
Jake Kim <imjakekim@gmail.com>
2022-07-27 08:08:01 -0700

    created first test for patron

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -1,7 +1,11 @@
 require 'patron'
 
 RSpec.describe Patron do
+  let(:patrion_1) {Patron.new("Bob", 20)}
   it 'Exists' do
-    
+    expect(patron_1).to be_an_instance_of(Patron)
+    expect(patron_1.name).to eq("Bob")
+    expect(patron_1.spending_money).to eq(20)
   end
+  
 end
