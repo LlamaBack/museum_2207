5bdcc78497a5ad7fab908f87078cd2c60c5346e8
Jake Kim <imjakekim@gmail.com>
2022-07-27 08:53:17 -0700

    wrote test for patrons_by_exhibit_interest

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -2,6 +2,12 @@ require './lib/museum'
 
 RSpec.describe Museum do
   let(:dmns) {Museum.new("Denver Museum of Nature and Science")}
+  let(:gems_and_minerals) {Exhibit.new({name: "Gems and Minerals", cost: 0})}
+  let(:dead_sea_scrolls) {Exhibit.new({name: "Dead Sea Scrolls", cost: 10})}
+  let(:imax) {Exhibit.new({name: "IMAX",cost: 15})}
+  let(:patron_1) {Patron.new("Bob", 20)}
+  let(:patron_2) {Patron.new("Sally", 20)}
+  let(:patron_3) {Patron.new("Johnny", 5)}
 
   it 'Exists and has instance variables' do
     expect(dmns).to be_an_instance_of(Museum)
@@ -10,9 +16,6 @@ RSpec.describe Museum do
   end
 
   it 'Can add exhibits' do
-    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
-    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
-    imax = Exhibit.new({name: "IMAX",cost: 15})
     dmns.add_exhibit(gems_and_minerals)
     dmns.add_exhibit(dead_sea_scrolls)
     dmns.add_exhibit(imax)
@@ -21,23 +24,42 @@ RSpec.describe Museum do
   end
 
   it 'Can reccomend exhibits' do
-    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
-    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
-    imax = Exhibit.new({name: "IMAX",cost: 15})
     dmns.add_exhibit(gems_and_minerals)
     dmns.add_exhibit(dead_sea_scrolls)
     dmns.add_exhibit(imax)
 
-    patron_1 = Patron.new("Bob", 20)
     patron_1.add_interest("Dead Sea Scrolls")
     patron_1.add_interest("Gems and Minerals")
 
-    patron_2 = Patron.new("Sally", 20)
     patron_2.add_interest("IMAX")
 
     expect(dmns.recommend_exhibits(patron_1)).to eq([gems_and_minerals, dead_sea_scrolls])
-
     expect(dmns.recommend_exhibits(patron_2)).to eq([imax])
+  end
+
+  it 'Can admit patrons and sort them by interests' do
+    patron_1 = Patron.new("Bob", 0)
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+
+    patron_1.add_interest("Gems and Minerals")
+    patron_1.add_interest("Dead Sea Scrolls")
+
+    patron_2.add_interest("Dead Sea Scrolls")
+
+    patron_3.add_interest("Dead Sea Scrolls")
+
+    dmns.admit(patron_1)
+    dmns.admit(patron_2)
+    dmns.admit(patron_3)
+
+    expect(dmns.patrons).to eq([patron_1, patron_2, patron_3])
+    expect(dmns.patrons_by_exhibit_interest).to eq({
+        gems_and_minerals => [patron_1],
+        dead_sea_scrolls => [patron_1, patron_2, patron_3],
+        imax => []
+      })
   end
 
 end
