c7b0ddb5c3b8db682ee9c9bafef1e880fb7f6761
Jake Kim <imjakekim@gmail.com>
2022-07-27 09:07:18 -0700

    fiinished admit and patrons_by_exhibit_interests method

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,8 +1,12 @@
+require 'exhibit'
+require 'patron'
+
 class Museum
-  attr_reader :name, :exhibits
+  attr_reader :name, :exhibits, :patrons
   def initialize(name)
     @name = name
     @exhibits = []
+    @patrons = []
   end
 
   def add_exhibit(exhibit)
@@ -13,5 +17,19 @@ class Museum
     @exhibits.find_all do |exhibit|
       patron.interests.include?(exhibit.name)
     end
+  end
+
+  def admit(patron)
+    @patrons << patron
+  end
+
+  def patrons_by_exhibit_interest
+    exhibit_interests = {}
+    @exhibits.each do |exhibit|
+      exhibit_interests[exhibit] = @patrons.find_all do |patron|
+        patron.interests.include?(exhibit.name)
+      end
+    end
+    exhibit_interests
   end
 end
